e8549bab6b4ae09498da35d82f92fb39ca6f78bb
Mikael Josias <mikaeljosias2002@gmail.com>
2023-01-18 16:12:44 -0300

    created the sign up page
    - added input fields
    - page css style
    - link to login page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import GlobalStyle from "../../assets/styles/globalStyle";
 import "../../assets/styles/fonts.css";
 import SignInPage from "../../pages/signInPage";
 import { AppContainer } from "./styled";
+import SignUpPage from "../../pages/signUpPage";
 
 export default function App(){
     return (
@@ -12,6 +13,7 @@ export default function App(){
             <BrowserRouter>
                 <Routes>
                     <Route exact path="/" element={<SignInPage/>} />
+                    <Route exact path="/cadastro" element={<SignUpPage/>} />
                 </Routes>
             </BrowserRouter>
         </AppContainer>
--- src/pages/signUpPage.jsx
+++ src/pages/signUpPage.jsx
@@ -4,24 +4,26 @@ import Input from "../components/form/input/input";
 import InputButton from "../components/form/inputButton/inputButton";
 import Logo from "../components/logo/logo";
 
-export default function SignInPage(){
+export default function SignUpPage(){
     return (
         <>
             <Logo/>
             <Form>
-                <Input type="email" placeholder="E-mail" required/>
-                <Input type="password" placeholder="Senha" required/>
-                <InputButton value="Entrar"/>
+                <Input type="text" placeholder="Nome"/>
+                <Input type="email" placeholder="E-mail"/>
+                <Input type="password" placeholder="Senha"/>
+                <Input type="password" placeholder="Confirme a senha"/>
+                <InputButton value="Cadastrar"/>
             </Form>
-            <Link to={"/cadastro"} style={{
+            <Link to="/" style={{
                 fontSize: "15px",
                 fontWeight: "700",
                 color: "white",
                 textDecoration: "none",
                 marginTop: "35px"
-                }}>
-                Primeira vez? Cadastre-se!
+            }}>
+                Já tem uma conta? Entre agora!
             </Link>
         </>
     );
-}
+} 
