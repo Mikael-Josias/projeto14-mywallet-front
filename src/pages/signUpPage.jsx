445a9c4f3e812d620c3cf55b3fce1108a115d521
Mikael Josias <mikaeljosias2002@gmail.com>
2023-01-20 15:21:22 -0300

    feat: client can now create a user account

--- src/pages/signUpPage.jsx
+++ src/pages/signUpPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import Form from "../components/form/form";
 import Input from "../components/form/input/input";
@@ -7,22 +8,32 @@ import InputButton from "../components/form/inputButton/inputButton";
 import Logo from "../components/logo/logo";
 
 export default function SignUpPage(){
+    const navigate = useNavigate();
 
     const [userData, setUserData] = useState({
         name: "",
         email: "",
         password: "",
-        confirmPassword: "",
     });
+
+    function registerNewUser(form){
+        form.preventDefault();
+
+        const promisse = axios.post(`${process.env.REACT_APP_API_URL}/signup`, userData);
+        promisse.then((res) => alert(res.data));
+        promisse.catch((err) => console.log(err));
+
+        navigate("/");
+    }
 
     return (
         <>
             <Logo/>
-            <Form submitFunction={null}>
-                <Input type="text" placeholder="Nome" onChange={e => setUserData({...userData, name: e.target.value})} pattern="^([ \u00c0-\u01ffa-zA-Z'\-])+$"/>
-                <Input type="email" placeholder="E-mail" onChange={e => setUserData({...userData, email: e.target.value})} pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"/>
-                <Input type="password" placeholder="Senha" onChange={e => setUserData({...userData, password: e.target.value})} pattern={userData.confirmPassword}/>
-                <Input type="password" placeholder="Confirme a senha" onChange={e => setUserData({...userData, confirmPassword: e.target.value})} pattern={userData.password}/>
+            <Form submitFunction={registerNewUser}>
+                <Input type="text" placeholder="Nome" onChange={e => setUserData({...userData, name: e.target.value})} required pattern="^([ \u00c0-\u01ffa-zA-Z'\-])+$"/>
+                <Input type="email" placeholder="E-mail" onChange={e => setUserData({...userData, email: e.target.value})} required pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"/>
+                <Input type="password" placeholder="Senha" onChange={e => setUserData({...userData, password: e.target.value})} required/>
+                <Input type="password" placeholder="Confirme a senha" required pattern={userData.password}/>
                 <InputButton value="Cadastrar"/>
             </Form>
             <Link to="/" style={{
